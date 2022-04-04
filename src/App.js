68f81f4eb6c3b2be13dbc85b3191100e24e950ba
nadarajan.s <nadarajan.s@gofrugal.com>
2022-04-04 10:04:13 +0530

    [SN] Sidebar and Sidebar Item implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
@@ -8,7 +9,11 @@ function App() {
           <div className='head'>
               {/* <h1>Fb</h1> */}
           </div>
-          <Header/>
+          <Header />
+          
+          <main className='flex bg-gray-100'>
+              <Sidebar />
+          </main>
       </div>
   );
 }
